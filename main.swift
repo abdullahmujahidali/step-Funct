f88c087ed91cd917d61ab8d0a0d82ef416562b49
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-10-18 14:43:44 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,13 @@
+func chooseStepFunction(backwards: Bool) -> (Int) -> Int {
+    func stepForward(input: Int) -> Int { return input + 1 }
+    func stepBackward(input: Int) -> Int { return input - 1 }
+    return backwards ? stepBackward : stepForward
+}
+var currentValue = -4
+let moveNearerToZero = chooseStepFunction(currentValue > 0)
+// moveNearerToZero now refers to the nested stepForward() function
+while currentValue != 0 {
+    print("\(currentValue)... ")
+    currentValue = moveNearerToZero(currentValue)
+}
+print("zero!")
